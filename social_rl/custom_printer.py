191bf8b31139fe2c0ea72a2a639ee114babb5cb8
nitsan57 <nitsan57@gmail.com>
2021-07-31 11:40:55 +0300

    update log file

--- social_rl/custom_printer.py
+++ social_rl/custom_printer.py
@@ -1,4 +1,11 @@
-def custom_printer(string, log_to="log.log"):
+import os
+
+
+def custom_printer(string):
+    if os.environ['redirect']:
+        log_to = os.environ['redirect']
+    else:
+        log_to = 'log.log'
     print(str(string))
     f = open(log_to, "a")
     f.write(str(string) + "\n")
